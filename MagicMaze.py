3d69f725b9208a8a32a6580dd34d409186d1ccc6
gianc <giancarlogavotti@gmail.com>
2019-09-19 22:45:41 +0200

    Function corrected

--- MagicMaze.py
+++ MagicMaze.py
@@ -1,21 +1,25 @@
 import random
-def life_counter(errors,lives):
-    if errors<10 :
-        errors +=1
-    elif errors==10:
-        lives+=1
-        errors=0
-    if lives==3:
+def life_counter(errors, lives):
+    if errors < 10:
+        errors += 1
+    elif errors == 10:
+        print("You lost one life")
+        lives += 1
+        errors = 0
+    if lives == 3:
         print("You lost")
         exit()
+    return lives, errors
+counter = 0
+errors = 0
+lives = 0
+
 directions=['N','S','E','W']
 random.shuffle(directions)
-#print(directions)# to know the pasword in advance
-print( end= "Welecome to de magic maze, which way do you want to go? ")
-counter=0
-errors=0
-lives=0
+#print(directions)# to know the password in advance
+print( end= "Welcome to de magic maze, which way do you want to go? ")
 while(True):
+
     guess= input("N,S,E,W\n")
     if (guess=='N' or guess=='S' or guess=='E' or guess=='W'):
         #print(directions[counter])#try to get what gets from the list each turn
@@ -27,10 +31,12 @@ while(True):
                 exit()
         else:
             print("Wrong answer")
-            life_counter(errors,lives)
+            errors,lives=life_counter(errors,lives)
+            print(lives)
     else:
         print("Not a valid answer")
-        life_counter(errors,lives)
+        errors,lives=life_counter(errors,lives)
+        print(lives)
 
 
 
